367127dc48aa33d680ea0a6bad57cff763a6472c
Daryl <daryl.9@gmail.com>
2022-01-23 08:50:37 +0800

    first commit

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    puts 'about is hit'
+  end
+end
---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,16 @@
+class PasswordResetsController < ApplicationController 
+    def new 
+
+    end
+
+    def create 
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            #send email
+            PasswordMailer.with(user: @user).reset.deliver_now
+        end
+        
+        redirect_to root_path, notice: "We sent a link to reset password"
+    end
+end
